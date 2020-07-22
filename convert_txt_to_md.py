cf876caaa83a99f477d13a2f7b734d88e07855e7
ryan.oconnor1@live.com <ryan.oconnor1@live.com>
2020-07-22 18:23:57 -0400

    little helper to import jot notes

---
+++ convert_txt_to_md.py
@@ -0,0 +1,14 @@
+import shutil
+import glob
+import os
+source = "/mnt/c/Users/ryano/Google Drive/Obsidian/Ryan's Brain/jot notes"
+files = glob.glob(os.path.sep.join([source,'*.txt']))
+for f in files:
+    
+    outF = open(f,"r+")
+    data = outF.read()
+    #outF.write(data)
+    outF.write('[[{date}]]'.format(date=f.split(os.path.sep)[-1].split('_')[0]))
+    outF.write('[[jot_capture]]')
+    outF.close()
+    shutil.move(f,f.replace('.txt','.md'))
